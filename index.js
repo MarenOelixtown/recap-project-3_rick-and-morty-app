55b58a62bc5229e3d1cf24efe3c7eab052bf8cc5
Maren <marenehlers@gmx.net>
2024-05-24 17:14:38 +0200

    create EventListener for navi-paginatio - bug

--- index.js
+++ index.js
@@ -10,21 +10,35 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-const maxPage = 1;
-const page = 1;
+console.log(prevButton);
+
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
+
+prevButton?.addEventlistener("click", () => {
+  if (page > 1) {
+    page--;
+    fetchCharacter();
+  }
+});
+nextButton?.addEventlistener("click", () => {
+  if (page < maxPage) {
+    page++;
+    fetchCharacter();
+  }
+});
 
 async function fetchCharacter() {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${page}`
+    );
     const data = await response.json();
+    console.log(data.info);
     const characters = data.results;
     characters.forEach((character) => {
       cardContainer.append(CharacterCard(character));
-      console.log(character.name);
-      console.log(character.status);
-      console.log(character.occurrences);
-      console.log(character);
     });
   } catch (error) {
     console.error("Fehler :", error);
