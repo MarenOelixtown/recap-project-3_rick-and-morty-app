c3bcad7252954c53a2b6cc99a59b8f221306f915
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-24 14:48:12 +0200

    restored changes on local machine

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -9,7 +8,6 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-// States
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
