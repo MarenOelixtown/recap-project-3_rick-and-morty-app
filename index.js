d289688a665ad289051746d42daf7890ddfb9d85
Maren <marenehlers@gmx.net>
2024-05-24 14:25:35 +0200

    create charactercard function and fetch-character-function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +13,21 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacter() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    const characters = data.results;
+    characters.forEach((character) => {
+      cardContainer.append(CharacterCard(character));
+      console.log(character.name);
+      console.log(character.status);
+      console.log(character.occurrences);
+      console.log(character);
+    });
+  } catch (error) {
+    console.error("Fehler :", error);
+  }
+}
+fetchCharacter();
